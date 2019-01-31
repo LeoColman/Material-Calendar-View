899f1031c899f67e71d820d8e81dcd6585cd9e8a
Kerooker <leonardo.colman98@gmail.com>
2019-01-31 20:36:13 -0200

    Stops calendar swipe

--- library/src/main/java/com/applandeo/materialcalendarview/extensions/CalendarViewPager.java
+++ library/src/main/java/com/applandeo/materialcalendarview/extensions/CalendarViewPager.java
@@ -3,6 +3,7 @@ package com.applandeo.materialcalendarview.extensions;
 import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -17,6 +18,16 @@ public class CalendarViewPager extends ViewPager {
 
     public CalendarViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return false;
     }
 
     //This method is needed to get wrap_content height for ViewPager
